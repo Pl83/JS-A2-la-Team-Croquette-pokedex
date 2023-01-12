bff12dadc432a9a907207c5c4409ed47780f2347
Pierre-louis Sans <pierre.louis.sans@gmail.com>
2023-01-12 23:19:07 +0100

    cross-air and other

--- script.js
+++ script.js
@@ -9,10 +9,13 @@ tabs.forEach(element => {
             var Npokemon = 1;
             let LeftArrow = document.querySelector('.left_arrow');
             let RightArrow = document.querySelector('.right_arrow');
+            let upArrow = document.querySelector('.up_arrow');
+            let downArrow = document.querySelector('.down_arrow');
 
                 fetch(`https://pokeapi.co/api/v2/pokemon/${Npokemon}`)
                 .then(response => response.json())
                 .then(data => {
+                    //console.log(data)
                     // récupère les nom, talents et capacités
                     const name = data.name;
                     const abilities = data.abilities.map(ability => ability.ability.name);
@@ -24,33 +27,30 @@ tabs.forEach(element => {
                         let randomIndex = Math.floor(Math.random() * moves.length);
                         randomMoves.push(moves[randomIndex]);
                     }
-
+                    //console.log(data)
                     document.querySelector('.pokemon_area').innerHTML ='';
-                    document.querySelector('.pokename').innerHTML ='';
-                    document.querySelector('.Ability').innerHTML ='';
-                    document.querySelector('.movesset').innerHTML ='';
+                    document.querySelector('.stats-display').innerHTML ='';
 
                     let diva = document.createElement('div')
                     diva.innerHTML = `<h2>${name}</h2><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${Npokemon}.png">`
                     document.querySelector('.pokemon_area').appendChild(diva)
 
-                    let divb = document.createElement('div')
-                    divb.innerHTML = `<h2>${name}</h2>`
-                    document.querySelector('.pokename').appendChild(divb)
-
-                    let divc = document.createElement('div')
-                    divc.innerHTML = `<p>Abilities: ${abilities.join(", ")}</p>`
-                    document.querySelector('.Ability').appendChild(divc)
-
-                    let divd = document.createElement('div')
-                    divd.innerHTML = `<p>Random moves: ${randomMoves.join(", ")}</p>`
-                    document.querySelector('.movesset').appendChild(divd)
+                    let dive = document.createElement('div')
+                    dive.innerHTML = `
+                    <h2>${name}</h2>
+                    <br>
+                    <p>Abilities: ${abilities.join(", ")}</p>
+                    <br>
+                    <p>Possible set: ${randomMoves.join(", ")}
+                    `
+                    document.querySelector('.stats-display').appendChild(dive)
 
             });
 
             LeftArrow.addEventListener('click', function () {
                 if (Npokemon > 1) {
                     Npokemon = Npokemon - 1;
+                    console.log('Current poke' + Npokemon)
                     fetch(`https://pokeapi.co/api/v2/pokemon/${Npokemon}`)
                 .then(response => response.json())
                 .then(data => {
@@ -67,25 +67,21 @@ tabs.forEach(element => {
                     }
 
                     document.querySelector('.pokemon_area').innerHTML ='';
-                    document.querySelector('.pokename').innerHTML ='';
-                    document.querySelector('.Ability').innerHTML ='';
-                    document.querySelector('.movesset').innerHTML ='';
+                    document.querySelector('.stats-display').innerHTML ='';
 
                     let diva = document.createElement('div')
                     diva.innerHTML = `<h2>${name}</h2><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${Npokemon}.png">`
                     document.querySelector('.pokemon_area').appendChild(diva)
 
-                    let divb = document.createElement('div')
-                    divb.innerHTML = `<h2>${name}</h2>`
-                    document.querySelector('.pokename').appendChild(divb)
-
-                    let divc = document.createElement('div')
-                    divc.innerHTML = `<p>Abilities: ${abilities.join(", ")}</p>`
-                    document.querySelector('.Ability').appendChild(divc)
-
-                    let divd = document.createElement('div')
-                    divd.innerHTML = `<p>Random moves: ${randomMoves.join(", ")}</p>`
-                    document.querySelector('.movesset').appendChild(divd)
+                    let dive = document.createElement('div')
+                    dive.innerHTML = `
+                    <h2>${name}</h2>
+                    <br>
+                    <p>Abilities: ${abilities.join(", ")}</p>
+                    <br>
+                    <p>Possible set: ${randomMoves.join(", ")}
+                    `
+                    document.querySelector('.stats-display').appendChild(dive)
 
             });
                 }
@@ -94,9 +90,11 @@ tabs.forEach(element => {
             RightArrow.addEventListener('click', function () {
                 if (Npokemon < 1008) {
                     Npokemon = Npokemon + 1;
+                    console.log('Current poke' + Npokemon)
                     fetch(`https://pokeapi.co/api/v2/pokemon/${Npokemon}`)
                 .then(response => response.json())
                 .then(data => {
+                    console.log(data)
                     // récupère les nom, talents et capacités
                     const name = data.name;
                     const abilities = data.abilities.map(ability => ability.ability.name);
@@ -110,29 +108,145 @@ tabs.forEach(element => {
                     }
 
                     document.querySelector('.pokemon_area').innerHTML ='';
-                    document.querySelector('.pokename').innerHTML ='';
-                    document.querySelector('.Ability').innerHTML ='';
-                    document.querySelector('.movesset').innerHTML ='';
+                    document.querySelector('.stats-display').innerHTML ='';
 
                     let diva = document.createElement('div')
                     diva.innerHTML = `<h2>${name}</h2><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${Npokemon}.png">`
                     document.querySelector('.pokemon_area').appendChild(diva)
 
-                    let divb = document.createElement('div')
-                    divb.innerHTML = `<h2>${name}</h2>`
-                    document.querySelector('.pokename').appendChild(divb)
-
-                    let divc = document.createElement('div')
-                    divc.innerHTML = `<p>Abilities: ${abilities.join(", ")}</p>`
-                    document.querySelector('.Ability').appendChild(divc)
-
-                    let divd = document.createElement('div')
-                    divd.innerHTML = `<p>Random moves: ${randomMoves.join(", ")}</p>`
-                    document.querySelector('.movesset').appendChild(divd)
+                    let dive = document.createElement('div')
+                    dive.innerHTML = `
+                    <h2>${name}</h2>
+                    <br>
+                    <p>Abilities: ${abilities.join(", ")}</p>
+                    <br>
+                    <p>Possible set: ${randomMoves.join(", ")}
+                    `
+                    document.querySelector('.stats-display').appendChild(dive)
 
             });
                 }
             });
+
+            upArrow.addEventListener('click', function () {
+                fetch(`https://pokeapi.co/api/v2/pokemon/${Npokemon}`)
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data)
+                    const name = data.name;
+                    const types = data.types.map(type => type.type.name);
+                    const height = data.height;
+                    const weight = data.weight;
+                    const statsName = data.stats.map(stat => stat.stat.name);
+                    const statsValue = data.stats.map(stat => stat.base_stat);
+                    const alternate = statsName.map((e,i) => `${e} : ${statsValue[i]}`);
+                    const abilities = data.abilities.map(ability => ability.ability.name);
+                    const moves = data.moves.map(move => move.move.name);
+                    const id = data.id;
+
+
+                    document.querySelector('.pokemon_area').innerHTML ='';
+                    document.querySelector('.stats-display').innerHTML ='';
+
+                    let diva = document.createElement('div')
+                    diva.innerHTML = `<h2>${name}</h2>
+                    <div class="imgdivup" >
+                        <div class="img-row">
+                            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png">
+                            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${id}.png"> 
+                        </div>
+                        <div class="img-row">
+                            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${id}.png">
+                            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/${id}.png">
+                        </div>
+                    </div>
+                    `
+                    document.querySelector('.pokemon_area').appendChild(diva)
+
+                    let divf = document.createElement('div')
+                    divf.innerHTML = `<h2>${name}</h2>
+                    <p>Abilities: ${abilities.join(", ")}</p>
+                    <br>
+                    <p>Types: ${types.join(", ")}</p>
+                    <br>
+                    <p>Height: ${height} décimétre</p>
+                    <p>Weight: ${weight} gramme</p>
+                    <br>
+                    <p>Stats: ${alternate.join(", ")}</p>
+                    <br>
+                    <p>All Moves: ${moves.join(", ")}</p>
+                    `
+                    document.querySelector('.stats-display').appendChild(divf)
+                });
+            });
+
+
+            downArrow.addEventListener('click', function () {
+                console.log('Current poke' + Npokemon)
+                document.querySelector('.pokemon_area').innerHTML ='';
+                document.querySelector('.stats-display').innerHTML ='';
+                fetch(`https://pokeapi.co/api/v2/pokemon-species/${Npokemon}/`)
+                    .then(response => response.json())
+                    .then(data => {
+                        fetch(data.evolution_chain.url)
+                            .then(response => response.json())
+                            .then(data => {
+                                let evolutions = [];
+                                function getEvolutions(evoChain) {
+                                    evolutions.push(evoChain.species.name);
+                                    if (evoChain.evolves_to.length > 0) {
+                                        for (let i = 0; i < evoChain.evolves_to.length; i++) {
+                                            getEvolutions(evoChain.evolves_to[i]);
+                                        }
+                                    }
+                                }
+                                getEvolutions(data.chain);
+                                console.log(evolutions);
+                                evolutions.forEach(evolution => {
+                                    fetch(`https://pokeapi.co/api/v2/pokemon/${evolution}/`)
+                                        .then(response => response.json())
+                                        .then(data => {
+                                            console.log(data);
+                                            
+                                            // récupère les nom, talents et capacités
+                                            const name = data.name;
+                                            const abilities = data.abilities.map(ability => ability.ability.name);
+                                            const moves = data.moves.map(move => move.move.name);
+                                            const id = data.id;
+
+                                            // Pick 4 random moves from the array
+                                            let randomMoves = [];
+                                            for (let i = 0; i < 4; i++) {
+                                                let randomIndex = Math.floor(Math.random() * moves.length);
+                                                randomMoves.push(moves[randomIndex]);
+                                            }
+                                            //console.log(data)
+                                            
+
+                                            let diva = document.createElement('div')
+                                            diva.innerHTML = `<h2>${name}</h2><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png">`
+                                            document.querySelector('.pokemon_area').appendChild(diva)
+
+                                            let dive = document.createElement('div')
+                                            dive.innerHTML = `                                            
+                                            <h2>${name}</h2>
+                                            <br>
+                                            <p>Abilities: ${abilities.join(", ")}</p>
+                                            <br>
+                                            <p>Possible set: ${randomMoves.join(", ")}
+                                            <br>
+                                            <br>
+                                            `
+                                            document.querySelector('.stats-display').appendChild(dive)
+                                        });
+                                });
+                                
+                            })
+                    })
+                    .catch(error => console.error(error));
+            });
+
+
         } else if (element.value === 'b'){
             let poke = []
             document.querySelector('.pokemon_area').innerHTML = '';
