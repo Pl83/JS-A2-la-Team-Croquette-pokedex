b3f9cc6e9cebf63d8227ff84013c1658889b5e34
Nalithdev <Ethan.delcroix@outlook.fr>
2023-01-13 09:17:06 +0100

    fix error of merging (losing check pokemon)

--- script.js
+++ script.js
@@ -414,7 +414,57 @@ function searchPokemon() {
     xhr.send();
 
 }
+function get_pokement(poke){
+    console.log(poke)
+    document.querySelector('.card').classList.add('hiddenn')
+    let allpokemon = fetch('https://pokeapi.co/api/v2/pokemon/'+ poke)
+    allpokemon.then(function (response) {
+        return response.json();
+    }).then(function (data) {
+        console.log(data);
 
+        let div = document.createElement('div')
+        div.classList.add('pokelone')
+        let div2 = document.createElement('div')
+        div2.innerHTML = `<div><button onclick="removehidden()">X</button><h2>${data.name}</h2><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${data.id}.png"></div>`
+        document.querySelector('.pokemon_area').appendChild(div)
+        document.querySelector('.pokelone').appendChild(div2)
+
+
+        // récupère les nom, talents et capacités
+        const name = data.name;
+        const abilities = data.abilities.map(ability => ability.ability.name);
+        const moves = data.moves.map(move => move.move.name);
+
+        // Pick 4 random moves from the array
+        let randomMoves = [];
+        for (let i = 0; i < 4; i++) {
+            let randomIndex = Math.floor(Math.random() * moves.length);
+            randomMoves.push(moves[randomIndex]);
+        }
+
+
+        document.querySelector('.pokename').innerHTML ='';
+        document.querySelector('.Ability').innerHTML ='';
+        document.querySelector('.movesset').innerHTML ='';
+
+
+        let divb = document.createElement('div')
+        divb.innerHTML = `<h2>${name}</h2>`
+        document.querySelector('.pokename').appendChild(divb)
+
+        let divc = document.createElement('div')
+        divc.innerHTML = `<p>Abilities: ${abilities.join(", ")}</p>`
+        document.querySelector('.Ability').appendChild(divc)
+
+        let divd = document.createElement('div')
+        divd.innerHTML = `<p>Random moves: ${randomMoves.join(", ")}</p>`
+        document.querySelector('.movesset').appendChild(divd)
+
+
+
+    });
+};
 
 function removehidden(){
     document.querySelector('.card').classList.remove('hiddenn')
