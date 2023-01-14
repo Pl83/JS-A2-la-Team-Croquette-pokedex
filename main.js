d99aff1caab0effd9dd9d63fbf0a1bf51255308c
Vorn-Hagen <thibaut.francois@edu.devinci.fr>
2023-01-14 12:44:35 +0100

    :bulb: I know where is the error

--- main.js
+++ main.js
@@ -1,46 +1,11 @@
-/*
-fetch('https://pokeapi.co/api/v2/pokemon?limit=20')
-            .then(response => response.json())
-            .then(data =>{
-                
-                data.results.forEach(poke => {
-                    //console.log(poke)
-
-                    fetch(poke["url"])
-                        .then(response => response.json())
-                        .then(data =>{
-                            //console.log(data)
-
-                            const inHtml = document.querySelector("#card")
-                            const box = document.createElement("section")
-                            const name = document.createElement("h3")
-                            const type = document.createElement("p")
-                            const img = document.createElement("img")
-                            //console.log(data["id"])
-
-                            box.setAttribute("class", data["types"][0]["type"]["name"]);
-
-                            name.setAttribute("id", data["id"]);
-                            name.innerHTML=data["name"]
-                            type.innerHTML=data["types"][0]["type"]["name"]
-                            img.setAttribute("src", "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"+data["id"]+".png");
-                            box.appendChild(name)
-                            box.appendChild(type)
-                            box.appendChild(img)
-                            inHtml.appendChild(box)
-                        });
-
-
-                    
-                });
-            })
-*/
 //new Audio('pop.mp3').play()
 
 // let utterance = new SpeechSynthesisUtterance("Hello world!");
 // utterance = new SpeechSynthesisUtterance("manges tes morts!");
 // speechSynthesis.speak(utterance);
 // console.log(speechSynthesis.getVoices())
+
+let oui = new Audio('sound_effects/its.mp3')
 
 guessGame()
 
@@ -60,6 +25,7 @@ fetch('https://pokeapi.co/api/v2/pokemon/'+rand)
         const inHtml = document.querySelector("#card")
         const box = document.createElement("section")
         const img = document.createElement("img")
+        img.setAttribute("class", "hide")
         //const can = document.createElement("canvas")
         //console.log(data["id"])
         inHtml.innerHTML=""
@@ -72,13 +38,13 @@ fetch('https://pokeapi.co/api/v2/pokemon/'+rand)
         box.appendChild(img)
         inHtml.appendChild(box)
 
-
         btn = document.querySelector("#btn")
         btn.addEventListener("click", ()=> {
             img.classList.remove("noir")
 
             rep =document.querySelector("#answer")
             inside = document.createElement("p")
+            img.classList.remove("hide")
             
             if (guess.value==="john cena") {
                 rep.innerHTML=""
@@ -88,17 +54,18 @@ fetch('https://pokeapi.co/api/v2/pokemon/'+rand)
                 const img = document.querySelector("img")
                 img.setAttribute("src", "https://static.posters.cz/image/1300/affiches-et-posters/wwe-john-cena-09-i8508.jpg");
             }else{
-                let oui = new Audio('sound_effects/its.mp3')
+                /*
                 oui.play()
 
-                oui.addEventListener('ended', (event) => {
-                    let utterance = new SpeechSynthesisUtterance(data["name"]);
-                    utterance.lang = 'en-US';
-                    speechSynthesis.speak(utterance);
-                })
-                
-
-                rep.innerHTML=""
+                //oui.addEventListener('ended', (event) => {
+                console.log(data["name"])
+                let utterance = new SpeechSynthesisUtterance(data["name"])
+                utterance.lang = 'en-US'
+                speechSynthesis.speak(utterance)
+                //})
+                */
+                console.log("A")
+                rep.innerHTML="" 
                 if(guess.value===data["name"]){
                     console.log("OUI")
                     inside.innerHTML="that's the right answer ! <br>It's "+data["name"]
